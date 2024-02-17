fe84fb05db632aa3a73c81429497b09512e54fd1
Jeffandrewo <andrewdandan80@gmail.com>
2024-02-17 13:48:33 +0800

    first

---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-2xl font-bold mb-5">
+        Welcome
+      </h1>
+      <p className="mb-5">
+        Design Layout Here!!!
+      </p>
+    </>
+  );
+}
