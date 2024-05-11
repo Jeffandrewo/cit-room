b4cf8b14b4b09f4da4f72afcbd47371ee8d27c67
Rgeane <tisoyrgeanee@gmail.com>
2024-05-11 12:53:36 +0800

    updated

--- app/NewsFeedItem.jsx
+++ app/NewsFeedItem.jsx
@@ -1,21 +1,44 @@
-"use client";
-import React, { useState } from "react";
-import { db } from "@/firebase";
-import { doc, deleteDoc} from "firebase/firestore";
-import { useUser } from "@clerk/nextjs";
+import React, { useState, useEffect } from 'react';
+import { db } from '@/firebase';
+import { doc, updateDoc } from 'firebase/firestore';
+import { useUser } from '@clerk/nextjs';
 
 const NewsFeedItem = ({ news }) => {
   const [showOptions, setShowOptions] = useState(false);
   const [expanded, setExpanded] = useState(false);
+  const [editing, setEditing] = useState(false);
+  const [editedTitle, setEditedTitle] = useState('');
+  const [editedDescription, setEditedDescription] = useState('');
   const { user } = useUser();
+
+  useEffect(() => {
+    // Set the initial state of editedTitle and editedDescription when news changes
+    setEditedTitle(news.postTitle);
+    setEditedDescription(news.postDescription);
+  }, [news]);
 
   const handleToggleOptions = () => {
     setShowOptions(!showOptions);
   };
 
-  const handleEdit = (id) => {
-    console.log(`Editing news item with id ${id}`);
+  const toggleExpandContent = () => {
+    setExpanded(!expanded);
   };
+
+  const handleEdit = async () => {
+    try {
+      const postRef = doc(db, 'posts', news.id);
+      await updateDoc(postRef, {
+        postTitle: editedTitle,
+        postDescription: editedDescription || '' // Ensure editedDescription is not undefined
+      });
+      console.log(`Successfully edited post with id ${news.id}`);
+      setEditing(false); // Exit editing mode
+    } catch (error) {
+      console.error(`Error editing post: ${error.message}`);
+    }
+  };
+  
 
   const handleDelete = async (id) => {
     try {
@@ -29,10 +52,6 @@ const NewsFeedItem = ({ news }) => {
     } catch (error) {
       console.error("Error deleting post:", error);
     }
-  };
-
-  const toggleExpandContent = () => {
-    setExpanded(!expanded);
   };
 
   return (
@@ -49,21 +68,39 @@ const NewsFeedItem = ({ news }) => {
           </h2>
           <p className="text-sm text-gray-500 mb-2">1h ago</p>
         </div>
-        <h3 className="text-xl font-bold mb-2">{news?.postTitle}</h3>
-        {expanded ? (
-          <p className="text-gray-700 mb-2">{news?.postDescription}</p>
+        {editing ? (
+          <>
+            <input
+              type="text"
+              value={editedTitle}
+              onChange={(e) => setEditedTitle(e.target.value)}
+              className="text-lg font-bold mb-2"
+            />
+            <textarea
+              value={editedDescription}
+              onChange={(e) => setEditedDescription(e.target.value)}
+              className="text-gray-700 mb-2"
+            />
+          </>
         ) : (
-          <p className="text-gray-700 mb-2">
-            {news?.postDescription?.slice(0, 100)}
-          </p>
-        )}
-        {news?.postDescription?.length > 100 && (
-          <button
-            className="text-blue-500 font-semibold text-sm"
-            onClick={toggleExpandContent}
-          >
-            {expanded ? "Show less" : "Show more"}
-          </button>
+          <>
+            <h3 className="text-xl font-bold mb-2">{news?.postTitle}</h3>
+            {expanded ? (
+              <p className="text-gray-700 mb-2">{news?.postDescription}</p>
+            ) : (
+              <p className="text-gray-700 mb-2">
+                {news?.postDescription?.slice(0, 100)}
+              </p>
+            )}
+            {news?.postDescription?.length > 100 && (
+              <button
+                className="text-blue-500 font-semibold text-sm"
+                onClick={toggleExpandContent}
+              >
+                {expanded ? "Show less" : "Show more"}
+              </button>
+            )}
+          </>
         )}
         {news?.photo_url && (
           <img
@@ -73,11 +110,26 @@ const NewsFeedItem = ({ news }) => {
           />
         )}
       </div>
-      {user && ( // Conditionally render based on isSignedIn
+      {user && (
         <div className="absolute top-2 right-2">
+          {editing ? (
+            <button
+              onClick={handleEdit}
+              className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-1 px-2 rounded-full"
+            >
+              Save
+            </button>
+          ) : (
+            <button
+              onClick={() => setEditing(true)}
+              className="bg-gray-200 hover:bg-gray-300 text-gray-600 font-bold py-1 px-2 rounded-full"
+            >
+              Edit
+            </button>
+          )}
           <button
             onClick={handleToggleOptions}
-            className="bg-gray-200 hover:bg-gray-300 text-gray-600 font-bold py-1 px-2 rounded-full"
+            className="bg-gray-200 hover:bg-gray-300 text-gray-600 font-bold py-1 px-2 ml-2 rounded-full"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -92,12 +144,6 @@ const NewsFeedItem = ({ news }) => {
           </button>
           {showOptions && (
             <div className="absolute top-10 right-2 bg-white shadow-md rounded-md">
-              <button
-                onClick={handleEdit}
-                className="block w-full text-left px-4 py-2 hover:bg-gray-100 text-sm"
-              >
-                Edit
-              </button>
               <button
                 onClick={() => handleDelete(news.id)}
                 className="block w-full text-left px-4 py-2 hover:bg-gray-100 text-sm"
