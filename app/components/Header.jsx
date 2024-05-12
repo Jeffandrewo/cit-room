956ca6ec44e4c12734ebaa6721dbc807cd47bf2b
EltherJohn <elther.john10@gmail.com>
2024-05-12 08:39:10 +0800

    design update
    admin-key design

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
   const { userId } = auth();
 
   return (
-    <nav className="[background-color:_#09cfcf] relative py-4 px-6  flex items-center justify-between mb-5 ">
+    <nav className="bg-gradient-to-r from-yellow-500 to-red-500 relative py-4 px-6 flex items-center justify-between mb-5">
       <div className="flex items-center">
         <Link href="/">
           <div className="text-lg uppercase font-bold text-white">
@@ -16,20 +16,27 @@ const Header = () => {
         </Link>
       </div>
       <div className="text-white flex items-center">
+
+      {userId && (
+          <Link
+            href="/dashboard"
+            className="text-lg font-bold hover:text-black mr-10"
+          >
+            Dashboard
+          </Link>
+        )}
+        
         <div className="relative inline-block text-left">
           <div className="group">
             <button className="text-lg font-bold hover:text-black mr-10 group-hover:text-black">
               Events
             </button>
             <div className="w-40 bg-white rounded-lg shadow-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300 absolute right-0 top-full mt-2" style={{ zIndex: 10 }}>
-              
-
               {userId && ( // Conditionally render the Booked Event link
                 <Link href="/bookingevent">
                   <div className="block px-4 py-2 text-sm text-gray-800 hover:bg-gray-200">Booked Event</div>
                 </Link>
               )}
-
               <Link href="/eventlist">
                 <div className="block px-4 py-2 text-sm text-gray-800 hover:bg-gray-200">Event List</div>
               </Link>
@@ -39,18 +46,6 @@ const Header = () => {
             </div>
           </div>
         </div>
-        <Link
-          href="/about-us"
-          className="text-lg font-bold hover:text-black mr-10"
-        >
-          About Us
-        </Link>
-        <Link
-          href="/contact-us"
-          className="text-lg font-bold hover:text-black mr-10"
-        >
-          Contact Us
-        </Link>
         <Link
           href="/calendar"
           className="text-lg font-bold hover:text-black mr-10"
@@ -63,20 +58,28 @@ const Header = () => {
         >
           News
         </Link>
-        {userId && (
-          <Link
-            href="/dashboard"
-            className="text-lg font-bold hover:text-black mr-10"
-          >
-            Dashboard
-          </Link>
-        )}
+    
         <Link
           href="/room-details"
           className="text-lg font-bold hover:text-black mr-10"
         >
           Room Details
         </Link>
+
+        <Link
+          href="/about-us"
+          className="text-lg font-bold hover:text-black mr-10"
+        >
+          About Us
+        </Link>
+        
+        <Link
+          href="/contact-us"
+          className="text-lg font-bold hover:text-black mr-10"
+        >
+          Contact Us
+        </Link>
+
         {userId && (
           <Link
             href="/admin-key"
@@ -101,6 +104,8 @@ const Header = () => {
       </div>
     </nav>
   );
+  
+  
 };
 
 export default Header;
--- app/sign-in/[[...sign-up]]/page.jsx
+++ app/sign-in/[[...sign-up]]/page.jsx
@@ -3,12 +3,15 @@ import React, { useState, useEffect } from 'react';
 import { SignIn } from '@clerk/nextjs';
 import { db } from "@/firebase";
 import { doc, getDoc } from 'firebase/firestore';
+import { IconButton, Input, InputAdornment } from '@mui/material';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
 
 const SignInPage = () => {
   const [key, setKey] = useState('');
   const [isValidKey, setIsValidKey] = useState(false); // State to track if key is valid
   const [showSignIn, setShowSignIn] = useState(false); // State to track whether to show SignIn component
-  
+  const [showText, setShowText] = useState(true); // State to track whether to show the text in the textbox
+
   useEffect(() => {
     const fetchAdminKey = async () => {
       try {
@@ -43,18 +46,47 @@ const SignInPage = () => {
     setKey(enteredKey);
   };
 
+  // Function to toggle visibility of text
+  const toggleShowText = () => {
+    setShowText(!showText);
+  };
+
   useEffect(() => {
     setShowSignIn(isValidKey);
   }, [isValidKey]);
 
   return (
-    <div style={{ position: 'relative', textAlign: 'center' }}>
-      <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '30px' }}>
-        <p style={{ marginRight: '10px' }}>ADMIN KEY:</p>
-        <input type="text" placeholder="Enter Admin Key" value={key} onChange={handleKeyChange} style={{ textAlign: 'center' }} />
-        {/*<button onClick={handleLogin} style={{ marginLeft: '10px' }}>Enter</button>*/}
+    <div style={{ textAlign: 'center', paddingTop: '50px' }}>
+      <div style={{ marginBottom: '30px', display: 'flex', alignItems: 'center' }}>
+        <label style={{ fontSize: '18px', fontWeight: 'bold', marginRight: '10px' }}>Admin Key:</label>
+        <Input 
+          type={showText ? 'text' : 'password'} 
+          placeholder="Enter Admin Key" 
+          value={key} 
+          onChange={handleKeyChange} 
+          style={{ 
+            padding: '10px', 
+            borderRadius: '5px', 
+            border: '1px solid #ccc', 
+            width: '300px', 
+            maxWidth: '100%', 
+            boxSizing: 'border-box',
+            marginRight: '10px' // Add margin to separate from the IconButton
+          }} 
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton onClick={toggleShowText} edge="end">
+                {showText ? <Visibility /> : <VisibilityOff />}
+              </IconButton>
+            </InputAdornment>
+          }
+        />
       </div>
-      {showSignIn && <SignIn style={{ position: 'absolute', top: '0', left: '50%', transform: 'translateX(-50%)' }} />}
+      {showSignIn && (
+        <div style={{ position: 'relative', maxWidth: '400px', margin: 'auto' }}>
+          <SignIn style={{ position: 'absolute', top: '0', left: '50%', transform: 'translateX(-50%)' }} />
+        </div>
+      )}
     </div>
   );
 };
